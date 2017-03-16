06f8a187d749ff27dd408bc9bcf2bcec48053307
Tameshwar Nirmalkar <tameshwar.nirmalkar@gmail.com>
2017-03-16 12:58:58 +0530

    remove large font and added some module

--- src/app/app.components.ts
+++ src/app/app.components.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 const path = require('path');
-console.log( 'Path', path);
 
 @Component({
   // moduleId: module.id+'',
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
+import './assets/css/main.css';
 
 console.info('app.environment:', app.environment);
 if (app.environment === 'production') {
