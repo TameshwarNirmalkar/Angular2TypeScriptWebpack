c1ae50de708a865214ad3e2d24f7addc0c6b4f3c
Tameshwar Nirmalkar <tameshwar.nirmalkar@gmail.com>
2017-03-17 08:11:26 +0530

    css and stylesheet loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,13 @@
 var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 var path = require('path');
 var ROOT_PATH = path.resolve(__dirname);
 var APP_PATH = path.resolve(ROOT_PATH, 'app');
-var BUILD_PATH = path.resolve(ROOT_PATH, 'dist');
+var BUILD_PATH = path.resolve(ROOT_PATH, 'build');
 
 module.exports = {
-
     entry: {
         'app': './src/main.ts',
         'polyfills': [
@@ -18,24 +18,29 @@ module.exports = {
     },
     output: {
         path: BUILD_PATH,
-        filename: '[name].[hash].js'
+        // filename: '[name].[hash].js',
+        filename: '[name].bundle.js'
     },
     module: {
         loaders: [
             { test: /\.component.ts$/, loader: 'ts-loader!angular2-template-loader' },
             { test: /\.ts$/, exclude: /\.component.ts$/, loader: 'ts-loader' },
             { test: /\.html$/, loader: 'raw-loader' },
-            { test: /\.css$/, loader: 'raw-loader' },
-            { test: /\.(jpe?g$|gif|png)$/, loader: 'url-loader?limit=10' }
+            // { test: /\.css$/, loader: 'raw-loader' },
+            // { test: /\.css$/, loader: 'style-loader!css-loader?file-loader?name=assets/css/[name].[ext]' },
+            { test: /\.css$/, loader: ExtractTextPlugin.extract({ fallback: 'style-loader', use: 'css-loader' }) },
+            { test: /\.(jpe?g$|gif|png)$/, loader: 'file-loader?name=assets/images/[name].[ext]?limit=1000' },
+            { test: /\.(eot|svg|ttf|woff|woff2)$/, loader: 'file-loader?name=assets/fonts/[name].[ext]' }
         ]
     },
     resolve: {
-        extensions: ['*', '.js', '.ts', '.html', '.css']
+        extensions: ['*', '.js', '.ts', '.css', '.html']
     },
     plugins: [
         new webpack.optimize.CommonsChunkPlugin({
             name: 'polyfills'
         }),
+        new ExtractTextPlugin('assets/css/bundle.css'),
         new HtmlWebpackPlugin({
             template: './src/index.html'
         }),
